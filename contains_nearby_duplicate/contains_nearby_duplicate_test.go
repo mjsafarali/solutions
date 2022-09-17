db000804f855bf1b720ed595742a3b760e48979a
mohammad safarali <mohammad.safarali@snapp.cab>
2022-09-17 04:32:23 +0430

    feat: contains nearby duplicate test

---
+++ contains_nearby_duplicate/contains_nearby_duplicate_test.go
@@ -0,0 +1,26 @@
+package contains_nearby_duplicate
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestContainsDuplicate(t *testing.T) {
+	tableTests := []struct {
+		nums                []int
+		k                   int
+		hasNearbyDuplicated bool
+	}{
+		{nums: []int{}, k: 10, hasNearbyDuplicated: false},
+		{nums: []int{1}, k: 2, hasNearbyDuplicated: false},
+		{nums: []int{1, 2, 3}, k: 3, hasNearbyDuplicated: false},
+		{nums: []int{1, 2, 3, 1}, k: 3, hasNearbyDuplicated: true},
+		{nums: []int{1, 0, 1, 1}, k: 1, hasNearbyDuplicated: true},
+		{nums: []int{1, 2, 3, 1, 2, 3}, k: 2, hasNearbyDuplicated: false},
+	}
+
+	for _, tt := range tableTests {
+		r := containsNearbyDuplicate(tt.nums, tt.k)
+		require.Equal(t, tt.hasNearbyDuplicated, r)
+	}
+}
