3c18415637370c53376c0481a8cfcd0517d8d6e8
mohammad safarali <mohammad.safarali@snapp.cab>
2022-09-17 04:32:16 +0430

    feat: contains nearby duplicate function

---
+++ contains_nearby_duplicate/contains_nearby_duplicate.go
@@ -0,0 +1,19 @@
+package contains_nearby_duplicate
+
+import "math"
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	if len(nums) == 0 || len(nums) == 1 {
+		return false
+	}
+
+	a := map[int]int{}
+	for i, num := range nums {
+		if j, found := a[num]; found && math.Abs(float64(i-j)) <= float64(k) {
+			return true
+		}
+		a[num] = i
+	}
+
+	return false
+}
