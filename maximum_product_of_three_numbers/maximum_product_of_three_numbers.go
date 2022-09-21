f57fc5b3a38935800546906ab24857e4a07b7f2d
mohammad safarali <mohammad.safarali@snapp.cab>
2022-09-21 18:24:03 +0430

    feat: find maximum_product_of_three_numbers func added

---
+++ maximum_product_of_three_numbers/maximum_product_of_three_numbers.go
@@ -0,0 +1,19 @@
+package maximum_product_of_three_numbers
+
+import (
+	"sort"
+)
+
+func maximumProduct(nums []int) int {
+	sort.Ints(nums)
+
+	return max(nums[len(nums)-1]*nums[len(nums)-2]*nums[len(nums)-3], nums[0]*nums[1]*nums[len(nums)-1])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
